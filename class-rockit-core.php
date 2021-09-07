360ab224dfa64515697ca99f2942eca2af3ab85c
Matt Stone <matt@rockandscissor.com>
2021-09-07 22:33:19 +0100

    Added shortcode function

--- class-rockit-core.php
+++ class-rockit-core.php
@@ -5,28 +5,39 @@ class Core
 {
     /**
      * Create a new instance
-     *
-     * @author Matt Stone <matt@rockandscissor.com>
-     *
-     * @since 1.0.0
      */
     public function __construct() {
+
+        add_shortcode( 'rockit', array( $this, 'create_shortcode' ) );
 
     }
 
     /**
      * Turn slug into camel case
      *
-     * @param string $slug
-     * @return string Camelcase version of the slug
-     *
-     * @author Matt Stone <matt@rockandscissor.com>
-     *
-     * @since 1.0.0
+     * @param   string  $slug
+     * @return  string  Camelcase version of the slug
      */
     public function rockit_slug_to_camelcase( string $slug ) : string
     {
         return str_replace( ' ', '', ucwords( str_replace( '-', ' ', $slug ) ) );
+    }
+
+    /**
+     * Shortcode to output various site settings in the content
+     *
+     * @param   array   $atts
+     * @return  string  Output of shortcode
+     */
+    function create_shortcode( $atts ) {
+
+        extract( shortcode_atts( array(
+            'option' => 'rockit',
+        ), $atts ) );
+
+        // return the field content
+        return "hello world";
+
     }
 
 }
